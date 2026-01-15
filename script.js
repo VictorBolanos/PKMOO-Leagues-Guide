7e148325fcb27e5af12774d5acc90f120ffc5a68
vbolanos-neurored <152314638+VictorBolanos@users.noreply.github.com>
2026-01-15 22:04:23 +0100

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,282 @@
+// Mapeo de pokémon por entrenador
+const pokemonByTrainer = {
+    'Lorelei': [
+        { name: 'Articuno', image: 'articuno.png' },
+        { name: 'Bronzong', image: 'bronzong.png' },
+        { name: 'Chansey', image: 'chansey.png' },
+        { name: 'Claydol', image: 'claydol.png' },
+        { name: 'Dragonite', image: 'dragonite.png' },
+        { name: 'Exeggutor', image: 'exeggutor.png' },
+        { name: 'Golduck', image: 'golduck.png' },
+        { name: 'Golurk', image: 'golurk.png' },
+        { name: 'Hariyama', image: 'hariyama.png' },
+        { name: 'Jynx', image: 'jynx.png' },
+        { name: 'Lapras', image: 'lapras.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Magnezone', image: 'magnezone.png' },
+        { name: 'Mantine', image: 'mantine.png' },
+        { name: 'Nidoking', image: 'nidoking.png' },
+        { name: 'Nidoqueen', image: 'nidoqueen.png' },
+        { name: 'Raichu', image: 'raichu.png' },
+        { name: 'Slowbro', image: 'slowbro.png' },
+        { name: 'Togekiss', image: 'togekiss.png' },
+        { name: 'Vileplume', image: 'vileplume.png' }
+    ],
+    'Bruno': [
+        { name: 'Aggron', image: 'aggron.png' },
+        { name: 'Blastoise', image: 'blastoise.png' },
+        { name: 'Darmanitan', image: 'darmanitan.png' },
+        { name: 'Eelektross', image: 'eelektross.png' },
+        { name: 'Electivire', image: 'electivire.png' },
+        { name: 'Gliscor', image: 'gliscor.png' },
+        { name: 'Heracross', image: 'heracross.png' },
+        { name: 'Hitmonchan', image: 'hitmonchan.png' },
+        { name: 'Hitmonlee', image: 'hitmonlee.png' },
+        { name: 'Hitmontop', image: 'hitmontop.png' },
+        { name: 'Infernape', image: 'infernape.png' },
+        { name: 'Krookodile', image: 'krookodile.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Machamp', image: 'machamp.png' },
+        { name: 'Metagross', image: 'metagross.png' },
+        { name: 'Muk', image: 'muk.png' },
+        { name: 'Rhyperior', image: 'rhyperior.png' },
+        { name: 'Salamence', image: 'salamence.png' },
+        { name: 'Seismitoad', image: 'seismitoad.png' },
+        { name: 'Slowbro', image: 'slowbro.png' },
+        { name: 'Staraptor', image: 'staraptor.png' },
+        { name: 'Steelix', image: 'steelix.png' },
+        { name: 'Torterra', image: 'torterra.png' },
+        { name: 'Ursaring', image: 'ursaring.png' }
+    ],
+    'Agatha': [
+        { name: 'Alakazam', image: 'alakazam.png' },
+        { name: 'Arbok', image: 'arbok.png' },
+        { name: 'Crobat', image: 'crobat.png' },
+        { name: 'Gengar', image: 'gengar.png' },
+        { name: 'Golduck', image: 'golduck.png' },
+        { name: 'Gyarados', image: 'gyarados.png' },
+        { name: 'Houndoom', image: 'houndoom.png' },
+        { name: 'Hydreigon', image: 'hydreigon.png' },
+        { name: 'Krookodile', image: 'krookodile.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Mandibuzz', image: 'mandibuzz.png' },
+        { name: 'Marowak', image: 'marowak.png' },
+        { name: 'Muk', image: 'muk.png' },
+        { name: 'Rotom Wash', image: 'rotom_agua.png' },
+        { name: 'Rotom Heat', image: 'rotom_fuego.png' },
+        { name: 'Rotom Frost', image: 'rotom_hielo.png' },
+        { name: 'Rotom Fan', image: 'rotom_ventilador.png' },
+        { name: 'Seismitoad', image: 'seismitoad.png' },
+        { name: 'Shiftry', image: 'shiftry.png' },
+        { name: 'Toxicroak', image: 'toxicroak.png' },
+        { name: 'Umbreon', image: 'umbreon.png' }
+    ],
+    'Lance': [
+        { name: 'Aerodactyl', image: 'aerodactyl.png' },
+        { name: 'Ampharos', image: 'ampharos.png' },
+        { name: 'Arbok', image: 'arbok.png' },
+        { name: 'Arcanine', image: 'arcanine.png' },
+        { name: 'Charizard', image: 'charizard.png' },
+        { name: 'Dragonite', image: 'dragonite.png' },
+        { name: 'Eelektross', image: 'eelektross.png' },
+        { name: 'Electivire', image: 'electivire.png' },
+        { name: 'Feraligatr', image: 'feraligatr.png' },
+        { name: 'Garchomp', image: 'garchomp.png' },
+        { name: 'Gyarados', image: 'gyarados.png' },
+        { name: 'Haxorus', image: 'haxorus.png' },
+        { name: 'Hydreigon', image: 'hydreigon.png' },
+        { name: 'Infernape', image: 'infernape.png' },
+        { name: 'Kingdra', image: 'kingdra.png' },
+        { name: 'Lapras', image: 'lapras.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Metagross', image: 'metagross.png' },
+        { name: 'Scizor', image: 'scizor.png' },
+        { name: 'Scrafty', image: 'scrafty.png' },
+        { name: 'Steelix', image: 'steelix.png' },
+        { name: 'Tyranitar', image: 'tyranitar.png' }
+    ],
+    'Blue': [
+        { name: 'Aerodactyl', image: 'aerodactyl.png' },
+        { name: 'Alakazam', image: 'alakazam.png' },
+        { name: 'Arcanine', image: 'arcanine.png' },
+        { name: 'Blastoise', image: 'blastoise.png' },
+        { name: 'Bronzong', image: 'bronzong.png' },
+        { name: 'Charizard', image: 'charizard.png' },
+        { name: 'Electivire', image: 'electivire.png' },
+        { name: 'Espeon', image: 'espeon.png' },
+        { name: 'Exeggutor', image: 'exeggutor.png' },
+        { name: 'Flareon', image: 'flareon.png' },
+        { name: 'Golem', image: 'golem.png' },
+        { name: 'Gyarados', image: 'gyarados.png' },
+        { name: 'Jolteon', image: 'jolteon.png' },
+        { name: 'Kingdra', image: 'kingdra.png' },
+        { name: 'Leafeon', image: 'leafeon.png' },
+        { name: 'Machamp', image: 'machamp.png' },
+        { name: 'Magnezone', image: 'magnezone.png' },
+        { name: 'Nidoking', image: 'nidoking.png' },
+        { name: 'Ninetales', image: 'ninetales.png' },
+        { name: 'Pidgeot', image: 'pidgeot.png' },
+        { name: 'Politoed', image: 'politoed.png' },
+        { name: 'Poliwrath', image: 'poliwrath.png' },
+        { name: 'Rhyperior', image: 'rhyperior.png' },
+        { name: 'Sandslash', image: 'sandslash.png' },
+        { name: 'Scizor', image: 'scizor.png' },
+        { name: 'Skarmory', image: 'skarmory.png' },
+        { name: 'Tyranitar', image: 'tyranitar.png' },
+        { name: 'Umbreon', image: 'umbreon.png' },
+        { name: 'Vaporeon', image: 'vaporeon.png' },
+        { name: 'Venusaur', image: 'venusaur.png' }
+    ],
+    'Will': [
+        { name: 'Absol', image: 'absol.png' },
+        { name: 'Altaria', image: 'altaria.png' },
+        { name: 'Bronzong', image: 'bronzong.png' },
+        { name: 'Chansey', image: 'chansey.png' },
+        { name: 'Claydol', image: 'claydol.png' },
+        { name: 'Clefable', image: 'clefable.png' },
+        { name: 'Electivire', image: 'electivire.png' },
+        { name: 'Empoleon', image: 'empoleon.png' },
+        { name: 'Espeon', image: 'espeon.png' },
+        { name: 'Exeggutor', image: 'exeggutor.png' },
+        { name: 'Flareon', image: 'flareon.png' },
+        { name: 'Gardevoir', image: 'gardevoir.png' },
+        { name: 'Girafarig', image: 'girafarig.png' },
+        { name: 'Golduck', image: 'golduck.png' },
+        { name: 'Golurk', image: 'golurk.png' },
+        { name: 'Grumpig', image: 'grumpig.png' },
+        { name: 'Hypno', image: 'hypno.png' },
+        { name: 'Jolteon', image: 'jolteon.png' },
+        { name: 'Jynx', image: 'jynx.png' },
+        { name: 'Liepard', image: 'liepard.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Magnezone', image: 'magnezone.png' },
+        { name: 'Mantine', image: 'mantine.png' },
+        { name: 'Slowbro', image: 'slowbro.png' },
+        { name: 'Typhlosion', image: 'typhlosion.png' },
+        { name: 'Umbreon', image: 'umbreon.png' },
+        { name: 'Xatu', image: 'xatu.png' }
+    ],
+    'Koga': [
+        { name: 'Ariados', image: 'ariados.png' },
+        { name: 'Bisharp', image: 'bisharp.png' },
+        { name: 'Crobat', image: 'crobat.png' },
+        { name: 'Ditto', image: 'ditto.png' },
+        { name: 'Electrode', image: 'electrode.png' },
+        { name: 'Ferrothorn', image: 'ferrothorn.png' },
+        { name: 'Floatzel', image: 'floatzel.png' },
+        { name: 'Forretress', image: 'forretress.png' },
+        { name: 'Gengar', image: 'gengar.png' },
+        { name: 'Gliscor', image: 'gliscor.png' },
+        { name: 'Hypno', image: 'hypno.png' },
+        { name: 'Lanturn', image: 'lanturn.png' },
+        { name: 'Lapras', image: 'lapras.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Magmortar', image: 'magmortar.png' },
+        { name: 'Muk', image: 'muk.png' },
+        { name: 'Nidoking', image: 'nidoking.png' },
+        { name: 'Nidoqueen', image: 'nidoqueen.png' },
+        { name: 'Ninetales', image: 'ninetales.png' },
+        { name: 'Parasect', image: 'parasect.png' },
+        { name: 'Rhyperior', image: 'rhyperior.png' },
+        { name: 'Samurott', image: 'samurott.png' },
+        { name: 'Scizor', image: 'scizor.png' },
+        { name: 'Sharpedo', image: 'sharpedo.png' },
+        { name: 'Skarmory', image: 'skarmory.png' },
+        { name: 'Skuntank', image: 'skuntank.png' },
+        { name: 'Starmie', image: 'starmie.png' },
+        { name: 'Stunfisk', image: 'stunfisk.png' },
+        { name: 'Swalot', image: 'swalot.png' },
+        { name: 'Tangrowth', image: 'tangrowth.png' },
+        { name: 'Tentacruel', image: 'tentacruel.png' },
+        { name: 'Venomoth', image: 'venomoth.png' },
+        { name: 'Weezing', image: 'weezing.png' }
+    ],
+    'Bruno (Johto)': [
+        { name: 'Aggron', image: 'aggron.png' },
+        { name: 'Blastoise', image: 'blastoise.png' },
+        { name: 'Blaziken', image: 'blaziken.png' },
+        { name: 'Darmanitan', image: 'darmanitan.png' },
+        { name: 'Eelektross', image: 'eelektross.png' },
+        { name: 'Electivire', image: 'electivire.png' },
+        { name: 'Gliscor', image: 'gliscor.png' },
+        { name: 'Golem', image: 'golem.png' },
+        { name: 'Heracross', image: 'heracross.png' },
+        { name: 'Hitmonchan', image: 'hitmonchan.png' },
+        { name: 'Hitmonlee', image: 'hitmonlee.png' },
+        { name: 'Hitmontop', image: 'hitmontop.png' },
+        { name: 'Kangaskhan', image: 'kangaskhan.png' },
+        { name: 'Krookodile', image: 'krookodile.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Luxray', image: 'luxray.png' },
+        { name: 'Machamp', image: 'machamp.png' },
+        { name: 'Metagross', image: 'metagross.png' },
+        { name: 'Muk', image: 'muk.png' },
+        { name: 'Poliwrath', image: 'poliwrath.png' },
+        { name: 'Salamence', image: 'salamence.png' },
+        { name: 'Seismitoad', image: 'seismitoad.png' },
+        { name: 'Slowbro', image: 'slowbro.png' },
+        { name: 'Staraptor', image: 'staraptor.png' },
+        { name: 'Steelix', image: 'steelix.png' },
+        { name: 'Torterra', image: 'torterra.png' }
+    ],
+    'Karen': [
+        { name: 'Absol', image: 'absol.png' },
+        { name: 'Blastoise', image: 'blastoise.png' },
+        { name: 'Electrode', image: 'electrode.png' },
+        { name: 'Excadrill', image: 'excadrill.png' },
+        { name: 'Feraligatr', image: 'feraligatr.png' },
+        { name: 'Flareon', image: 'flareon.png' },
+        { name: 'Gallade', image: 'gallade.png' },
+        { name: 'Garchomp', image: 'garchomp.png' },
+        { name: 'Gengar', image: 'gengar.png' },
+        { name: 'Gyarados', image: 'gyarados.png' },
+        { name: 'Honchkrow', image: 'honchkrow.png' },
+        { name: 'Houndoom', image: 'houndoom.png' },
+        { name: 'Hydreigon', image: 'hydreigon.png' },
+        { name: 'Leafeon', image: 'leafeon.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Luxray', image: 'luxray.png' },
+        { name: 'Mismagius', image: 'mismagius.png' },
+        { name: 'Primeape', image: 'primeape.png' },
+        { name: 'Quagsire', image: 'quagsire.png' },
+        { name: 'Rhyperior', image: 'rhyperior.png' },
+        { name: 'Sableye', image: 'sableye.png' },
+        { name: 'Salamence', image: 'salamence.png' },
+        { name: 'Slowbro', image: 'slowbro.png' },
+        { name: 'Tyranitar', image: 'tyranitar.png' },
+        { name: 'Umbreon', image: 'umbreon.png' },
+        { name: 'Victreebel', image: 'victreebel.png' },
+        { name: 'Vileplume', image: 'vileplume.png' },
+        { name: 'Weavile', image: 'weavile.png' }
+    ],
+    'Lance (Johto)': [
+        { name: 'Aerodactyl', image: 'aerodactyl.png' },
+        { name: 'Ampharos', image: 'ampharos.png' },
+        { name: 'Arbok', image: 'arbok.png' },
+        { name: 'Arcanine', image: 'arcanine.png' },
+        { name: 'Charizard', image: 'charizard.png' },
+        { name: 'Dragonite', image: 'dragonite.png' },
+        { name: 'Eelektross', image: 'eelektross.png' },
+        { name: 'Electivire', image: 'electivire.png' },
+        { name: 'Feraligatr', image: 'feraligatr.png' },
+        { name: 'Flygon', image: 'flygon.png' },
+        { name: 'Garchomp', image: 'garchomp.png' },
+        { name: 'Gyarados', image: 'gyarados.png' },
+        { name: 'Haxorus', image: 'haxorus.png' },
+        { name: 'Hydreigon', image: 'hydreigon.png' },
+        { name: 'Infernape', image: 'infernape.png' },
+        { name: 'Kangaskhan', image: 'kangaskhan.png' },
+        { name: 'Kingdra', image: 'kingdra.png' },
+        { name: 'Lapras', image: 'lapras.png' },
+        { name: 'Lucario', image: 'lucario.png' },
+        { name: 'Metagross', image: 'metagross.png' },
+        { name: 'Scizor', image: 'scizor.png' },
+        { name: 'Scrafty', image: 'scrafty.png' },
+        { name: 'Steelix', image: 'steelix.png' },
+        { name: 'Tyranitar', image: 'tyranitar.png' }
+    ]
+};
+
 // Mapeo de entrenadores por región
 const trainersByRegion = {
     kanto: [
@@ -40,9 +319,11 @@ const trainersByRegion = {
 // Elementos del DOM
 const regionButtons = document.querySelectorAll('.region-btn');
 const trainersContainer = document.getElementById('trainersContainer');
+let currentRegion = 'kanto'; // Variable para guardar la región actual
 
 // Función para mostrar entrenadores
 function showTrainers(region) {
+    currentRegion = region; // Guardar región actual
     const trainers = trainersByRegion[region];
     
     // Limpiar contenedor
@@ -71,7 +352,7 @@ function showTrainers(region) {
         
         // Añadir event listener para mostrar pokémon líderes
         trainerCard.addEventListener('click', () => {
-            showLeaderPokemons(trainer.name);
+            showLeaderPokemons(trainer.name, currentRegion);
         });
         
         trainersContainer.appendChild(trainerCard);
@@ -84,12 +365,29 @@ function showTrainers(region) {
 }
 
 // Función para mostrar pokémon líderes del entrenador seleccionado
-function showLeaderPokemons(trainerName) {
+function showLeaderPokemons(trainerName, region) {
     const leaderSection = document.getElementById('leaderPokemonsSection');
     const leaderTitle = document.getElementById('leaderPokemonsTitle');
     const leaderContainer = document.getElementById('leaderPokemonsContainer');
     
     if (!leaderSection || !leaderTitle || !leaderContainer) return;
+    
+    // Buscar pokémon del entrenador
+    // Manejar casos especiales: Bruno y Lance están en Kanto y Johto
+    let pokemons = null;
+    
+    if (trainerName === 'Bruno' && region === 'johto') {
+        pokemons = pokemonByTrainer['Bruno (Johto)'];
+    } else if (trainerName === 'Lance' && region === 'johto') {
+        pokemons = pokemonByTrainer['Lance (Johto)'];
+    } else {
+        pokemons = pokemonByTrainer[trainerName];
+    }
+    
+    // Si aún no hay pokémon, usar array vacío
+    if (!pokemons || pokemons.length === 0) {
+        pokemons = [];
+    }
     
     // Actualizar título
     leaderTitle.textContent = `${trainerName} - Pokémons Líder`;
@@ -97,19 +395,19 @@ function showLeaderPokemons(trainerName) {
     // Limpiar contenedor
     leaderContainer.innerHTML = '';
     
-    // Crear 4 tarjetas vacías
-    for (let i = 0; i < 4; i++) {
+    // Crear tarjetas para cada pokémon
+    pokemons.forEach(pokemon => {
         const pokemonCard = document.createElement('div');
         pokemonCard.className = 'leader-pokemon-card';
         pokemonCard.innerHTML = `
             <div class="leader-pokemon-image-wrapper">
-                <img src="" alt="" class="leader-pokemon-image">
-                <div class="leader-pokemon-name">Pokémon ${i + 1}</div>
+                <img src="img/pokes/${pokemon.image}" alt="${pokemon.name}" class="leader-pokemon-image">
+                <div class="leader-pokemon-name">${pokemon.name}</div>
             </div>
         `;
         
         leaderContainer.appendChild(pokemonCard);
-    }
+    });
     
     // Mostrar sección con animación
     leaderSection.style.display = 'block';
